3216d817bbb0c7d35ef6e3befcc03423e259e888
SARAVANAN <144509113+SARAVANAN15032003@users.noreply.github.com>
2024-10-11 16:40:47 +0530

    Create First Missing Positive.java

---
+++ First Missing Positive.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+    Arrays.sort(nums);
+int v=1;
+HashSet<Integer> set=new HashSet<>();
+for(int i:nums)set.add(i);
+   while(true){
+    if(!(set.contains(v)))break;v++;
+   }
+     return v;
+    }
+}
