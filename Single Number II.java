701b87a594f68d1c6423761ee503e8945a1315e2
SARAVANAN <144509113+SARAVANAN15032003@users.noreply.github.com>
2024-10-18 00:05:42 +0530

    Update Single Number II.java

--- Single Number II.java
+++ Single Number II.java
@@ -1,18 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-HashMap<Integer,Integer> hm=new HashMap<>();
-for(int i:nums){
-    if(hm.containsKey(i))
-    hm.put(i,hm.get(i)+1);
-    else
-    hm.put(i,1);
+Arrays.sort(nums);
+for(int i=0;i<nums.length-1;i=i+3){
+    if(nums[i]!=nums[i+2])
+    return nums[i];
 }
-int y=0;
- for (int key : hm.keySet()) {
- if (hm.get(key) == 1) {
-    y=key;
-    break;}
-        }
-        return y;
+return nums[nums.length-1];
     }
 }
