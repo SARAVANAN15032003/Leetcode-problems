6230f936b6a82b9acb57db52575bb2fb6d36eace
SARAVANAN <144509113+SARAVANAN15032003@users.noreply.github.com>
2024-11-05 23:42:14 +0530

    Create String Matching in an Array.java

---
+++ String Matching in an Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        ArrayList<String> list=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+for(int j=0;j<words.length;j++){
+    if(j!=i){
+        if(words[i].contains(words[j])){
+           if(!list.contains(words[j]))
+            list.add(words[j]);
+        }
+    }
+}
+        }
+        return list;
+    }
+}
