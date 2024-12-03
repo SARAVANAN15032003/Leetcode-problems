35320e0eec47f1392550601f7cfd0658a838c8b7
SARAVANAN <144509113+SARAVANAN15032003@users.noreply.github.com>
2024-12-03 19:25:03 +0530

    Create Number Complement.java

---
+++ Number Complement.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findComplement(int num) {
+        StringBuilder sb=new StringBuilder(Integer.toBinaryString(num));
+        for(int i=0;i<sb.length();i++){
+            if(sb.charAt(i)=='1')
+            sb.setCharAt(i,'0');
+            else
+            sb.setCharAt(i,'1');
+        }
+        return Integer.parseInt(sb.toString(),2);
+            }
+}
