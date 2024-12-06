0a9284ed592f1928a604feb9d7f49e0bb4a2287d
SARAVANAN <144509113+SARAVANAN15032003@users.noreply.github.com>
2024-12-06 12:00:47 +0530

    Create Valid Triangle Number.java

---
+++ Valid Triangle Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int c=0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            for(int j=i+1;j<nums.length-1;j++){
+                for(int k=j+1;k<nums.length;k++){
+                    if(nums[i]+nums[j]>nums[k]&&nums[i]+nums[k]>nums[j]&&nums[k]+nums[j]>nums[i])
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
